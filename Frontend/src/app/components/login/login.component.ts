3330800327583847ecb1840fe6814289e0e2b4ef
TTMVP-At <tom.pirich@outlook.com>
2021-05-07 20:58:39 +0200

    Added formgroup

--- Frontend/src/app/components/login/login.component.ts
+++ Frontend/src/app/components/login/login.component.ts
@@ -1,6 +1,8 @@
+
 import { HttpClient } from '@angular/common/http';
 import { Component, Injectable, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import { UserDto } from '../../../../../Shared/user.dto';
 import { AuthenticationService } from '../../services/authentication.service'
 
@@ -12,29 +14,39 @@ import { AuthenticationService } from '../../services/authentication.service'
 })
 @Injectable()
 export class LoginComponent implements OnInit {
-
+  private formBuilder: FormBuilder | undefined;
   public username: string | undefined;
   public password: string | undefined;
-  public loginForm: FormGroup | undefined;
-  public isLoading : Boolean = false;
-  constructor(private httpClient : HttpClient,
-                       private authenticationService :  AuthenticationService) {
+  public loginForm: FormGroup;
 
-                       }
+  public isLoading: Boolean = false;
 
-  ngOnInit(): void {}
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+    ) {
+      if (this.authenticationService.currentUserValue)
+        this.router.navigate(['/']);
+
+      this.loginForm = new FormGroup({});
+    }
+
+  ngOnInit(): void {
+    
+  }
 
   public onSubmit(): void {
     this.isLoading = true;
-    let user = new UserDto(this.username || "", this.password || "");
-    this.authenticationService.login(user)
-    .toPromise()
-    .then((response) =>{
-      alert('Success');
-    })
-    .catch((error) =>{
-      alert('Error');
-    });
+    let user = new UserDto(this.username || '', this.password || '');
+    this.authenticationService
+      .login(user)
+      .toPromise()
+      .then((response) => {
+        alert('Success');
+      })
+      .catch((error) => {
+        alert('Error');
+      });
 
     this.isLoading = false;
   }
