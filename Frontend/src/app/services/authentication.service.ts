e500e1924971cfa9d6bec588a5913e39d58b4b37
TTMVP-AT <tom.pirich@outlook.com>
2021-05-07 21:55:00 +0200

    Authentication service is responsible for:
    * Login: Storing session in local storage
    * Logout: Deletion session from local storage => not yet implemented.

---
+++ Frontend/src/app/services/authentication.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+
+import { UserDto } from '../../../../Shared/user.dto'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+    private currentUserSubject: BehaviorSubject<UserDto>;
+    public currentUser: Observable<UserDto>;
+  constructor(private httpClient : HttpClient) { 
+      this.currentUserSubject = new BehaviorSubject<UserDto>(
+      JSON.parse(localStorage.getItem('currentUser') || '{}')
+    );
+
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue(): UserDto {
+    return this.currentUserSubject.value;
+  }
+
+
+  login(user : UserDto) {
+    alert("GG");
+    return this.httpClient.post('http://localhost:41005/user/add', user.fullName)
+        .pipe(
+          map((user) => {
+          // store user details and jwt token in local storage to keep user logged in between page refreshes
+          
+          localStorage.setItem('currentUser', JSON.stringify(user));
+          let userData = user as UserDto;
+          this.currentUserSubject.next(userData);
+          return user;
+        })
+      );
+  }
+
+    logout() {
+    // remove user from local storage and set current user to null
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(new UserDto("",""));
+  }
+}
